6bca6de6b20dc6ed0b6b0bc6b000887f254e518a
Heitor Silva Medeiros <silvaheitor080@gmail.com>
2026-04-04 20:00:57 -0300

    a

--- src/com/cursodoyoutube/maratonajava/introducao/Teste.java
+++ src/com/cursodoyoutube/maratonajava/introducao/Teste.java
@@ -1,7 +1,9 @@
 package com.cursodoyoutube.maratonajava.introducao;
 
 public class Teste {
-
+/* Imprime algo
+Comentário sem utilidade
+ */
     public static void main(String[] args){
         System.out.println("Teste");
     }
