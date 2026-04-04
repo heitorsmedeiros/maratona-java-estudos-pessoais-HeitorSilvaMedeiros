b221197b19e038815993b6d0dea1aff34be7e080
Heitor Silva Medeiros <silvaheitor080@gmail.com>
2026-04-04 20:16:05 -0300

    a

---
+++ src/com/cursodoyoutube/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,10 @@
+package com.cursodoyoutube.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+
+        int age = 10;
+        double orcamentoCompleto = 54458.26;
+        System.out.println(age);
+    }
+}
