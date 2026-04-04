850503588d214886d9d6d36ab15612c7f7dedbce
Heitor Silva Medeiros <silvaheitor080@gmail.com>
2026-04-04 20:59:21 -0300

    a

---
+++ src/com/cursodoyoutube/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,18 @@
+package com.cursodoyoutube.maratonajava.introducao;
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String nome = "José da Silva";
+        String endereco = "Rua do Hospício, 123, Centro, Recife-PE";
+        double salario = 1621.49D;
+        String data = "08/04/2026";
+
+        System.out.println("Eu "+nome+", morando no endereço "+endereco+", confirmo que recebi o salário de "+salario+", na data "+data);
+
+
+
+
+
+
+    }
+}
