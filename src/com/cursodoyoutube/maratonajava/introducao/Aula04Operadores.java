179c33df8becc83bdf0180b41f7ed78f8c93a800
Heitor Silva Medeiros <silvaheitor080@gmail.com>
2026-04-04 21:06:13 -0300

    a

--- src/com/cursodoyoutube/maratonajava/introducao/Aula04Operadores.java
+++ src/com/cursodoyoutube/maratonajava/introducao/Aula04Operadores.java
@@ -4,6 +4,6 @@ public class Aula04Operadores {
     public static void main(String[] args) {
         int numero01 = 10;
         int numero02 = 20;
-        System.out.println(numero02-numero01);
+        System.out.println(numero02+numero01+"Valor "+numero02+numero01);
     }
 }
