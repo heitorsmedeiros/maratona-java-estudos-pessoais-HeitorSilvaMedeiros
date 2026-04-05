54120e7e850b8baddb136fd9cfd3688e109dfc4f
Heitor Silva Medeiros <silvaheitor080@gmail.com>
2026-04-05 17:15:30 -0300

    a

--- src/com/cursodoyoutube/maratonajava/introducao/Aula04Operadores.java
+++ src/com/cursodoyoutube/maratonajava/introducao/Aula04Operadores.java
@@ -4,6 +4,11 @@ public class Aula04Operadores {
     public static void main(String[] args) {
         int numero01 = 10;
         int numero02 = 20;
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        float valorPlaystation = 5000F;
+        boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
         System.out.println(numero02+numero01+"Valor "+numero02+numero01);
+        System.out.println("isPlaystationCincoCompravel "+isPlaystationCincoCompravel);
     }
 }
